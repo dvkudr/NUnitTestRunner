b199a7644bd99e90f75891d5d53793cccd69765a
Dmitrii Kudriavtcev <Dmitrii.Kudriavtcev@ipreo.com>
2017-12-15 17:49:01 +0300

    second version

--- TestSample/TestAddClass.cs
+++ TestSample/TestAddClass.cs
@@ -6,17 +6,20 @@ namespace TestSample
     [TestFixture]
     public class TestAddClass
     {
-        [TestCase(0, 1, 1)]
-        [TestCase(1, 0, 1)]
-        [TestCase(1, 2, 3)]
-        [TestCase(2, 1, 3)]
-        [TestCase(2, 2, 5)]
-        [TestCase(2, 3, 5)]
+        [TestCaseSource(nameof(testData))]
         public void TestAdditionCases(int x1, int x2, int expectedResult)
         {
             var actualResult = x1 + x2;
 
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        private static object[] testData = new object[]
+        {
+            new int [] { 1, 1, 2},
+            new int [] { 1, 2, 3},
+            new int [] { 2, 2, 5},
+            new int [] { 2, 3, 5},
+        };
     }
 }
